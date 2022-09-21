81b9bb41279bd814003fd47464aecb8aac1580b6
Douglas Mennella <douglas.mennella@gmail.com>
2022-09-21 21:08:57 +0900

    more tests

--- ngnk-mode-test.el
+++ ngnk-mode-test.el
@@ -4,17 +4,58 @@
   "Test prefilter"
   (unwind-protect
       (progn
-        (setq ngnk-buffer-limit nil)
-        (setq orig ngnk-max-output-length)
-        (setq ngnk-max-output-length 2)
-        (should (equal (ngnk-preout-filter "1\n2\n3\n\aprompt") "1\n2\n...prompt"))
-        (should (equal (ngnk-preout-filter "1\n2\n3\n\a\nprompt") "1\n2\n...\nprompt"))
-        (should (equal (ngnk-preout-filter "1\n2\n\aprompt") "1\n2\n...prompt"))
-        (should (equal (ngnk-preout-filter "1\n2\aprompt") "1\n2prompt"))
-        (should (equal (ngnk-preout-filter "1\n2\n\a\n\nprompt\nextra") "1\n2\n...\n\nprompt\nextra"))
-        (should (equal (ngnk-preout-filter "1\n\aprompt") "1\nprompt"))
-        (should (equal (ngnk-preout-filter "1\n\a prompt") "1\n prompt")))
+        (let ((chk (lambda (s r)
+                     (should (equal (ngnk-preout-filter s) r))
+                     (should (equal ngnk-buffer-limit nil)))))
+          (setq ngnk-buffer-limit nil)
+          (setq orig ngnk-max-output-length)
+          (setq ngnk-max-output-length 2)
+          (funcall chk
+                   "1\n2\n3\n\aprompt"
+                   "1\n2\n...prompt")
+          (funcall chk
+                   "1\n2\n3\n\a\nprompt"
+                   "1\n2\n...\nprompt")
+          (funcall chk
+                   "1\n2\n\aprompt"
+                   "1\n2\n...prompt")
+          (funcall chk
+                   "1\n2\aprompt"
+                   "1\n2prompt")
+          (funcall chk
+                   "1\n2\n\a\n\nprompt\nextra"
+                   "1\n2\n...\n\nprompt\nextra")
+          (funcall chk
+                   "1\n\aprompt"
+                   "1\nprompt")
+          (funcall chk
+                   "1\n\a prompt"
+                   "1\n prompt")
+          (setq ngnk-max-output-length 0)
+          (funcall chk
+                   "1\n2\n3\n\aprompt"
+                   "1\n2\n3\nprompt")
+          (funcall chk
+                   "1\n2\n3\n\aprompt"
+                   "1\n2\n3\nprompt")
+          (funcall chk
+                   "1\n2\n\aprompt"
+                   "1\n2\nprompt")
+          (funcall chk
+                   "1\n2\aprompt"
+                   "1\n2prompt")
+          (funcall chk
+                   "1\n2\n\a\n\nprompt\nextra"
+                   "1\n2\n\n\nprompt\nextra")
+          (funcall chk
+                   "1\n\aprompt"
+                   "1\nprompt")
+          (funcall chk
+                   "1\n\a prompt"
+                   "1\n prompt")))
     (progn
+      ;; remove buffer local variable
       (makunbound 'ngnk-buffer-limit)
+      ;; restore custom setting
       (setq ngnk-max-output-length orig)
       )))
